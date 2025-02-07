f02dba7775f8c6b00399a5247f15cb6c1748d927
Deepak-2598 <deepaksurya25998@gmail.com>
2025-02-07 19:07:26 +0530

    added optimization

---
+++ src/Components/Optimization.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import { Suspense,lazy } from 'react'
+
+const Optimization = () => {
+  return (
+    <div>Optimization perfomance like </div>
+  )
+}
+
+export default Optimization
