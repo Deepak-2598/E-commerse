cce3551f19c309ca1ba70b0ba1499995fb4ebfeb
Deepak-2598 <deepaksurya25998@gmail.com>
2025-02-07 20:00:41 +0530

    added about.tsx

---
+++ src/Components/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        About
+        <p>Deecart is e-commerse website launced in 2024</p>
+    </div>
+  )
+}
+
+export default About
